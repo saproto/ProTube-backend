7b3ee48ebea65d2a7a14f7c3eca046e47b4d88d2
Max Liebe <amalox@outlook.com>
2022-01-31 11:26:11 +0100

    Removed redundant else statements

--- routes/api.js
+++ routes/api.js
@@ -7,64 +7,60 @@ const admin_remote = require('../modules/admin-remote')
 //api authentication with bearer token -everything below is authorized
 this.apiRouter.use(bearerToken());
 this.apiRouter.use(function (req, res, next) {
-    if (req.token == process.env.API_KEY) {
-      next();
-    } else {
-      res.status(401).json({
+    if (req.token == process.env.API_KEY) next();
+    res.status(401).json({
         success: false,
         message: 'Not Authorized for this API'
-      });
-    }
-  });
+    });
+});
 
 //volume management
 this.apiRouter.get('/setvolume/:type/:volume', (req, res) => {
-  volume = parseInt(req.params.volume);
-  type = req.params.type;
+    volume = parseInt(req.params.volume);
+    type = req.params.type;
 
-  if (typeof volume !== 'number' || isNaN(volume) || volume < 0 || volume > 100){
-    return res.json({
-      success: false,
-      message: 'incorrect volume'
-    });
-  }
-  if(type == "radio" || type == "master" || type == "youtube"){
-    return admin_remote.changeVolume(type, volume);
-  } else{
+    if (typeof volume !== 'number' || isNaN(volume) || volume < 0 || volume > 100) {
+        return res.json({
+            success: false,
+            message: 'incorrect volume'
+        });
+    }
+    if (type === "radio" || type === "master" || type === "youtube") {
+        return admin_remote.changeVolume(type, volume);
+    }
     res.json({
-      success: false,
-      modified: type,
-      message: 'incorrect type'
+        success: false,
+        modified: type,
+        message: 'incorrect type'
     });
-  }
 });
 
 //fetch current item playing
 this.apiRouter.get('/currentlyplaying', (req, res) => {
-  return res.json({
-    success: true,
-    video: protube.getCurrentVideo(),
-    status: protube.getStatus(),
-    message: 'currently playing'
-  });
+    return res.json({
+        success: true,
+        video: protube.getCurrentVideo(),
+        status: protube.getStatus(),
+        message: 'currently playing'
+    });
 });
 
 //fetch queue
 this.apiRouter.get('/queue', (req, res) => {
-  return res.json({
-    success: true,
-    video_queue: protube.getQueue(),
-    total_duration: protube.getQueueDuration(),
-    message: 'queue - bug: returns video even though it has ended'
-  });
+    return res.json({
+        success: true,
+        video_queue: protube.getQueue(),
+        total_duration: protube.getQueueDuration(),
+        message: 'queue - bug: returns video even though it has ended'
+    });
 });
 
 //unmatched paths
-this.apiRouter.use(function(req, res) {
-  return res.json({
-    success: false,
-    message: 'not a path'
-  });
+this.apiRouter.use(function (req, res) {
+    return res.json({
+        success: false,
+        message: 'not a path'
+    });
 });
 
 //allow/block queue input
