1d2bc6779936b3141aa072cc03636314624c3bae
Max Liebe <amalox@outlook.com>
2022-03-20 13:49:58 +0100

    Added some things to gitignore + todo list

--- modules/authenticator.js
+++ modules/authenticator.js
@@ -27,7 +27,7 @@ exports.validateClient = async (_cookies, screencode_correct=false) => {
             return true;
         }
         // Cookie is new, validate it at the webserver
-        let userdata = await fetch('https://localhost:8085/userdetails', {
+        let userdata = await fetch('https://localhost:8080/api/protube/userdetails', {
         headers: {
             cookie: `proto_session=${proto_cookie}`
         }});
